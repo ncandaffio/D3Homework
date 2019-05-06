8ffce086b5511b89d49fd64d202006d57f0503c4
ncandaffio <44885428+ncandaffio@users.noreply.github.com>
2019-05-06 18:35:28 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
 // @TODO: YOUR CODE HERE!
+d3.csv("assets/data/data.csv", function(error, data){
+    if (error) return console.warn(error);
+    var abbr = data.mod(value => value.abbr)
+    console.log(abbr);
+});
