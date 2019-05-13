30d9111c7c95bd8779a213e545ca2e99ab6ef141
ncandaffio <44885428+ncandaffio@users.noreply.github.com>
2019-05-13 15:50:38 -0400

    more complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,6 +144,7 @@ function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
 
   var toolTip = d3.tip()
     .attr("class", "tooltip")
+    .style("background-color", "#bfbfbf")
     .offset([80, -60])
     .html(function(d) {
       return (`${d.state}<br>${xlabel} ${d[chosenXAxis]}<br>${ylabel} ${d[chosenYAxis]}`);
